2789aa2def9ec15d4dd3717ce00105a5cdea452e
Kyle Godbey <me@kyle.ee>
2022-05-26 22:18:11 -0400

    add CV to new public repo

---
+++ CV/recompile.sh
@@ -0,0 +1,2 @@
+rm KyleGodbeyCV.aux KyleGodbeyCV.bbl KyleGodbeyCV.blg KyleGodbeyCV-blx.bib KyleGodbeyCV.log KyleGodbeyCV.run.xml
+pdflatex KyleGodbeyCV; bibtex KyleGodbeyCV; pdflatex KyleGodbeyCV; pdflatex KyleGodbeyCV
